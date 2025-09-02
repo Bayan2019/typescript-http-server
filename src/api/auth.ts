54218e595bdcc6d550709cea69e7d4a3d04a5b1b
Bayan2019 <saparbayevabt@gmail.com>
2025-09-02 23:51:16 +0500

    starting with authentication still

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -1,7 +1,17 @@
-import { hashSync } from "bcrypt";
+import { hashSync, compare } from "bcrypt";
 // import { hash } from "crypto";
 
 export function hashPassword(password: string) {
     const hashedPassword = hashSync(password, 10)
     return hashPassword
+}
+
+export async function verifyPassword(plainPassword: string, hashedPasswordFromDB: string): Promise<boolean> {
+  try {
+    const isMatch = await compare(plainPassword, hashedPasswordFromDB);
+    return isMatch;
+  } catch (error) {
+    console.error('Error comparing password:', error);
+    return false;
+  }
 }
